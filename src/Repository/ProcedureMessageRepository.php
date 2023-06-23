25dca5972e329e90cd528f527516db117486af35
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-06-23 13:04:49 +0200

    feature (refs T33340): fix repository and remove extra methods

--- src/Repository/ProcedureMessageRepository.php
+++ src/Repository/ProcedureMessageRepository.php
@@ -2,15 +2,11 @@
 
 namespace DemosEurope\DemosplanAddon\XBeteiligung\Repository;
 
-use DemosEurope\DemosplanAddon\Contracts\Entities\ProcedureInterface;
 use DemosEurope\DemosplanAddon\XBeteiligung\Entity\ProcedureMessage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 use Exception;
-use InvalidArgumentException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProcedureMessageRepository extends ServiceEntityRepository
@@ -31,18 +27,6 @@ class ProcedureMessageRepository extends ServiceEntityRepository
     }
 
     /**
-     * @param ProcedureMessage $ProcedureMessage
-     *
-     * @throws ORMException
-     * @throws OptimisticLockException
-     */
-    public function deleteObject(ProcedureMessage $ProcedureMessage): void
-    {
-        $this->getEntityManager()->remove($ProcedureMessage);
-        $this->getEntityManager()->flush();
-    }
-
-    /**
      * @param $ProcedureMessageId
      * @return ProcedureMessage
      */
@@ -51,29 +35,18 @@ class ProcedureMessageRepository extends ServiceEntityRepository
         return $this->find($ProcedureMessageId);
     }
 
-    public function getAll()
-    {
-        return $this->createQueryBuilder('ProcedureMessage')
-            ->andWhere('ProcedureMessage.deleted = :parameter')
-            ->setParameter('parameter', false)
-            ->getQuery()
-            ->getResult();
-    }
-
     /**
-     * Create a ProcedureMessage with necessary properties
-     * and validate it.
-     *
-     * @throws InvalidArgumentException
+     * @param string $procedureMessageID
+     * @return string
+     * @throws Exception
      */
-    public function createProcedureMessage(ProcedureInterface $procedure): ProcedureMessage
+    public function getProcedureMessage(string $procedureMessageID): string
     {
-
-        $procedureMessage = new ProcedureMessage($procedure);
-
-        $this->validator->validate($procedureMessage);
-
-        return $procedureMessage;
+        /** @var ProcedureMessage $procedureMessage **/
+        $procedureMessage = $this->findBy(['id' => $procedureMessageID]);
+        $procedureMessage->setRequestCount();
+        $this->updateObject($procedureMessage);
+        return $procedureMessage->getMessage();
     }
 
     /**
@@ -96,44 +69,6 @@ class ProcedureMessageRepository extends ServiceEntityRepository
             $this->logger->warning('update SingleMessage failed Reason: ', [$e]);
             throw $e;
         }
-    }
-
-    /**
-     * @param $entityId
-     * @return mixed
-     * @throws Exception
-     */
-    public function delete($entityId)
-    {
-        try {
-            return $this->createQueryBuilder('ProcedureMessage')
-                ->andWhere('ProcedureMessage.id', 'id')
-                ->set('ProcedureMessage.deleted' , ':parameter')
-                ->setParameter('id', $entityId)
-                ->setParameter('parameter', true)
-                ->getQuery()
-                ->getResult();
-        } catch (Exception $e) {
-            $this->logger->warning('delete SingleMessage failed Reason: ', [$e]);
-            throw $e;
-        }
-    }
-
-    public function persist(ProcedureMessage $ProcedureMessage)
-    {
-        $this->entityManager->persist($ProcedureMessage);
-    }
-
-    public function persistEntities(array $entities): void
-    {
-        foreach ($entities as $entity) {
-            $this->getEntityManager()->persist($entity);
-        }
-    }
-
-    public function flushEverything(): void
-    {
-        $this->getEntityManager()->flush();
     }
 
 }
