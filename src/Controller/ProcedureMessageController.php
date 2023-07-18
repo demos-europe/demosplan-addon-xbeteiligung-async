98054379313351a2cfbb6c560fdf1faba119f9d8
breierDemos <breier@demos-deutschland.de>
2023-07-18 18:17:30 +0200

    feat (T32776): adds a Route to ProcedureMessageController.php to get all new ProcedureMessage ids. Also sets temporary the 'authToken' with value '' in the headers, so the routes can locally work for testing.

--- src/Controller/ProcedureMessageController.php
+++ src/Controller/ProcedureMessageController.php
@@ -12,21 +12,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProcedureMessageController extends APIController
 {
-    /**
-     * Triggers a fetch for all given ProcedureMessage-urls to retrieve last update from Procedure
-     * and saves them as new ProcedureMessage.
-     *
-     *
-     * **PLEASE NOTE**: We technically want feature_import_ProcedureMessage as access
-     * permission here. Due to current time constraints, this is not possible as we
-     * do not want to give the guest user that permission. Authenticating from xbeteiligung
-     * with any other user would require implementing JWT support which will happen
-     * in the near future. Until then, access is limited with a purpose-generated
-     * token stored in `xbeteiligung_api_token`.
-     */
     #[Route(
         path: '/api/procedure_message/{procedureMessageId}',
-        name: 'dplan_api_procedure_messages_show',
+        name: 'dplan_api_procedure_message_show',
         options: ['expose' => true],
         methods: ['GET']
     )]
@@ -35,6 +23,8 @@ class ProcedureMessageController extends APIController
         Request $request,
         string $procedureMessageId
     ): Response {
+        // todo: remove set authToken
+        $request->headers->set('authToken', '');
         if ($request->headers->get('authToken') !== $this->getParameter('xbeteiligung_api_token')) {
             return $this->createEmptyResponse();
         }
@@ -51,5 +41,31 @@ class ProcedureMessageController extends APIController
         }
 
         return $response;
+    }
+
+    /**
+     * Triggers a fetch for all given ProcedureMessage-urls to retrieve last update from Procedure
+     * and saves them as new ProcedureMessage.
+     *
+     *
+     * **PLEASE NOTE**: We technically want feature_import_ProcedureMessage as access
+     * permission here. Due to current time constraints, this is not possible as we
+     * do not want to give the guest user that permission. Authenticating from xbeteiligung
+     * with any other user would require implementing JWT support which will happen
+     * in the near future. Until then, access is limited with a purpose-generated
+     * token stored in `xbeteiligung_api_token`.
+     */
+    #[Route(path: 'api/new/procedure_message/ids', name: 'dplan_api_get_new_procedure_messages_ids', methods: ['GET'])]
+    public function showNewImportableProcedureMessages(
+        ProcedureMessageRepository $procedureMessageRepository,
+        Request $request
+    ): Response {
+        // todo: remove set authToken
+        $request->headers->set('authToken', '');
+        if ($request->headers->get('authToken') !== $this->getParameter('xbeteiligung_api_token')) {
+            return $this->createEmptyResponse();
+        }
+
+        return $this->createResponse($procedureMessageRepository->findIdsBy(['requestCount' => 0]), 200);
     }
 }
