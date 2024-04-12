c10b06fdee21c41a93fe622ce22c1aaf3dda1ca8
breierDemos <106672028+breierDemos@users.noreply.github.com>
2024-04-12 11:49:12 +0200

    feat (DPLAN-11355): Add missing documents in 302 messages. (24)
    * fix (DPLAN-11355): Add missing documents in 302 messages.
    
    * feat (DPLAN-11355): adjust changelog

--- src/Logic/XBeteiligungService.php
+++ src/Logic/XBeteiligungService.php
@@ -472,6 +472,7 @@ class XBeteiligungService
             DateTime::createFromInterface($procedure->getStartDate())->sub(new DateInterval('P7D'))
         );
         $participationType->setDurchgang(1);
+        $participationType->setAnlagen($this->planningDocumentsLinkCreator->getPlanningDocuments($procedure));
         // In rog we have currently no "Geltungsbereich zeichnen" option under "Planungsdokumente und Planzeichnung".
         $participationType->setGeltungsbereich('');
         // *************************************************************************************************************
