dfc20d6aa7d1d96d481f6e0e9fa8b5ae4a28f20a
breierDemos <breier@demos-deutschland.de>
2025-03-14 19:09:33 +0100

    feat (refs AB#25310): Add generated php classes for xbeteiligung. 4

---
+++ src/Soap/Schema/XBeteiligung/RaumordnungAktualisieren0302.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung;
+
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungAktualisieren0302\RaumordnungAktualisieren0302AnonymousPHPType;
+
+/**
+ * Class representing RaumordnungAktualisieren0302
+ *
+ * Diese Nachricht wird an die, die Beteiligung durchführende, Behörde versendet und enthält die zur Aktualisierung des Beteiligungsverfahrens der Raumordnung notwendigen Metadaten.
+ */
+class RaumordnungAktualisieren0302 extends RaumordnungAktualisieren0302AnonymousPHPType
+{
+}
+
