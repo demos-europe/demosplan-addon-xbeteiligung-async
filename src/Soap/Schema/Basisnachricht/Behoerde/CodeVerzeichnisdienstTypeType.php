b9c0dd8770e8235305e747ce61cd26293718bd8c
breierDemos <breier@demos-deutschland.de>
2025-03-14 18:56:58 +0100

    feat (refs AB#25310): Add generated php classes for basisnachricht.

---
+++ src/Soap/Schema/Basisnachricht/Behoerde/CodeVerzeichnisdienstTypeType.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Basisnachricht\Behoerde;
+
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Code\CodeType;
+
+/**
+ * Class representing CodeVerzeichnisdienstTypeType
+ *
+ *
+ * XSD Type: Code.VerzeichnisdienstType
+ */
+class CodeVerzeichnisdienstTypeType extends CodeType
+{
+    /**
+     * @var string $listURI
+     */
+    private $listURI = null;
+
+    /**
+     * @var string $listVersionID
+     */
+    private $listVersionID = null;
+
+    /**
+     * @var string $code
+     */
+    private $code = null;
+
+    /**
+     * Gets as listURI
+     *
+     * @return string
+     */
+    public function getListURI()
+    {
+        return $this->listURI;
+    }
+
+    /**
+     * Sets a new listURI
+     *
+     * @param string $listURI
+     * @return self
+     */
+    public function setListURI($listURI)
+    {
+        $this->listURI = $listURI;
+        return $this;
+    }
+
+    /**
+     * Gets as listVersionID
+     *
+     * @return string
+     */
+    public function getListVersionID()
+    {
+        return $this->listVersionID;
+    }
+
+    /**
+     * Sets a new listVersionID
+     *
+     * @param string $listVersionID
+     * @return self
+     */
+    public function setListVersionID($listVersionID)
+    {
+        $this->listVersionID = $listVersionID;
+        return $this;
+    }
+
+    /**
+     * Gets as code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Sets a new code
+     *
+     * @param string $code
+     * @return self
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+}
+
