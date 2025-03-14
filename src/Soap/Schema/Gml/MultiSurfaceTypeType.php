f77c35f3d139c056f7e6f277ff01f2d97bf67bca
breierDemos <breier@demos-deutschland.de>
2025-03-14 19:00:44 +0100

    feat (refs AB#25310): Add generated php classes for gml. 5

---
+++ src/Soap/Schema/Gml/MultiSurfaceTypeType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Gml;
+
+/**
+ * Class representing MultiSurfaceTypeType
+ *
+ *
+ * XSD Type: MultiSurfaceType
+ */
+class MultiSurfaceTypeType extends AbstractGeometricAggregateTypeType
+{
+    /**
+     * @var \DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Gml\SurfaceMember[] $surfaceMember
+     */
+    private $surfaceMember = [
+        
+    ];
+
+    /**
+     * Adds as surfaceMember
+     *
+     * @return self
+     * @param \DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Gml\SurfaceMember $surfaceMember
+     */
+    public function addToSurfaceMember(\DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Gml\SurfaceMember $surfaceMember)
+    {
+        $this->surfaceMember[] = $surfaceMember;
+        return $this;
+    }
+
+    /**
+     * isset surfaceMember
+     *
+     * @param int|string $index
+     * @return bool
+     */
+    public function issetSurfaceMember($index)
+    {
+        return isset($this->surfaceMember[$index]);
+    }
+
+    /**
+     * unset surfaceMember
+     *
+     * @param int|string $index
+     * @return void
+     */
+    public function unsetSurfaceMember($index)
+    {
+        unset($this->surfaceMember[$index]);
+    }
+
+    /**
+     * Gets as surfaceMember
+     *
+     * @return \DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Gml\SurfaceMember[]
+     */
+    public function getSurfaceMember()
+    {
+        return $this->surfaceMember;
+    }
+
+    /**
+     * Sets a new surfaceMember
+     *
+     * @param \DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Gml\SurfaceMember[] $surfaceMember
+     * @return self
+     */
+    public function setSurfaceMember(?array $surfaceMember = null)
+    {
+        $this->surfaceMember = $surfaceMember;
+        return $this;
+    }
+}
+
---
+++ src/Soap/Schema/Gml/PointTypeType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Gml;
+
+/**
+ * Class representing PointTypeType
+ *
+ *
+ * XSD Type: PointType
+ */
+class PointTypeType extends AbstractGeometricPrimitiveTypeType
+{
+    /**
+     * @var float[] $pos
+     */
+    private $pos = null;
+
+    /**
+     * Adds as pos
+     *
+     * @return self
+     * @param float $pos
+     */
+    public function addToPos($pos)
+    {
+        $this->pos[] = $pos;
+        return $this;
+    }
+
+    /**
+     * isset pos
+     *
+     * @param int|string $index
+     * @return bool
+     */
+    public function issetPos($index)
+    {
+        return isset($this->pos[$index]);
+    }
+
+    /**
+     * unset pos
+     *
+     * @param int|string $index
+     * @return void
+     */
+    public function unsetPos($index)
+    {
+        unset($this->pos[$index]);
+    }
+
+    /**
+     * Gets as pos
+     *
+     * @return float[]
+     */
+    public function getPos()
+    {
+        return $this->pos;
+    }
+
+    /**
+     * Sets a new pos
+     *
+     * @param float[] $pos
+     * @return self
+     */
+    public function setPos(array $pos)
+    {
+        $this->pos = $pos;
+        return $this;
+    }
+}
+
