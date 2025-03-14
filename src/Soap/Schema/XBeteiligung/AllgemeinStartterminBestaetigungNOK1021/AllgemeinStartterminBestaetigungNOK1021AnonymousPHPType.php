d7731cf6ef237b1cdcbd70e9129f76669fdd6f15
breierDemos <breier@demos-deutschland.de>
2025-03-14 19:12:54 +0100

    feat (refs AB#25310): Add generated php classes for xbeteiligung. 6

---
+++ src/Soap/Schema/XBeteiligung/AllgemeinStartterminBestaetigungNOK1021/AllgemeinStartterminBestaetigungNOK1021AnonymousPHPType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\AllgemeinStartterminBestaetigungNOK1021;
+
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Basisnachricht\G2g\NachrichtG2GTypeType;
+
+/**
+ * Class representing AllgemeinStartterminBestaetigungNOK1021AnonymousPHPType
+ */
+class AllgemeinStartterminBestaetigungNOK1021AnonymousPHPType extends NachrichtG2GTypeType
+{
+    /**
+     * Diese Nachricht unterrichtet über einen Fehler bei der Verarbeitung der Originalnachricht.
+     *
+     * @var \DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\NachrichteninhaltTemplateNOKType $nachrichteninhalt
+     */
+    private $nachrichteninhalt = null;
+
+    /**
+     * Gets as nachrichteninhalt
+     *
+     * Diese Nachricht unterrichtet über einen Fehler bei der Verarbeitung der Originalnachricht.
+     *
+     * @return \DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\NachrichteninhaltTemplateNOKType
+     */
+    public function getNachrichteninhalt()
+    {
+        return $this->nachrichteninhalt;
+    }
+
+    /**
+     * Sets a new nachrichteninhalt
+     *
+     * Diese Nachricht unterrichtet über einen Fehler bei der Verarbeitung der Originalnachricht.
+     *
+     * @param \DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\NachrichteninhaltTemplateNOKType $nachrichteninhalt
+     * @return self
+     */
+    public function setNachrichteninhalt(\DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\NachrichteninhaltTemplateNOKType $nachrichteninhalt)
+    {
+        $this->nachrichteninhalt = $nachrichteninhalt;
+        return $this;
+    }
+}
+
