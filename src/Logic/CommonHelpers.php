d115e54b93429e7b559f12a64bd35852d3e52b7e
Moritz Mandler <mandler@demos-deutschland.de>
2025-04-09 17:53:30 +0200

    f-AB#15105 add more header classes

--- src/Logic/CommonHelpers.php
+++ src/Logic/CommonHelpers.php
@@ -19,14 +19,32 @@ use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\AllgemeinSt
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\AllgemeinStellungnahmeGeloescht0709;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\AllgemeinStellungnahmeNeuabgegeben0701;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalAktualisieren0402;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalAktualisierenNOK0422;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalAktualisierenOK0412;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalInitiieren0401;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalInitiierenNOK0421;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalInitiierenOK0411;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalLoeschen0409;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalLoeschenNOK0429;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\KommunalLoeschenOK0419;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungAktualisieren0202;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungAktualisierenNOK0222;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungAktualisierenOK0212;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungInitiieren0201;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungInitiierenNOK0221;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungInitiierenOK0211;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungLoeschen0209;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungLoeschenNOK0229;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\PlanfeststellungLoeschenOK0219;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungAktualisieren0302;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungAktualisierenNOK0322;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungAktualisierenOK0312;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungInitiieren0301;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungInitiierenNOK0321;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungInitiierenOK0311;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungLoeschen0309;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungLoeschenNOK0329;
+use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\RaumordnungLoeschenOK0319;
 use DOMDocument;
 use InvalidArgumentException;
 use Psr\Log\LoggerInterface;
@@ -139,6 +157,132 @@ class CommonHelpers
                 'name' => 'planfeststellung.Loeschen.0209',
                 'author' => 'Demosplan',
                 'recipient' => 'K3',
+            ],
+        KommunalInitiierenOK0411::class =>
+            [
+                'code' => '0411',
+                'name' => 'kommunal.Initiieren.OK.0411',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        KommunalInitiierenNOK0421::class =>
+            [
+                'code' => '0421',
+                'name' => 'kommunal.Initiieren.NOK.0421',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        KommunalAktualisierenOK0412::class =>
+            [
+                'code' => '0412',
+                'name' => 'kommunal.Aktualisieren.OK.0412',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        KommunalAktualisierenNOK0422::class =>
+            [
+                'code' => '0422',
+                'name' => 'kommunal.Aktualisieren.NOK.0422',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        KommunalLoeschenOK0419::class =>
+            [
+                'code' => '0419',
+                'name' => 'kommunal.Loeschen.OK.0419',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        KommunalLoeschenNOK0429::class =>
+            [
+                'code' => '0429',
+                'name' => 'kommunal.Loeschen.NOK.0429',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        PlanfeststellungInitiierenOK0211::class =>
+            [
+                'code' => '0211',
+                'name' => 'planfeststellung.Initiieren.OK.0211',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        PlanfeststellungAktualisierenOK0212::class =>
+            [
+                'code' => '0212',
+                'name' => 'planfeststellung.Aktualisieren.OK.0212',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        PlanfeststellungLoeschenOK0219::class =>
+            [
+                'code' => '0219',
+                'name' => 'planfeststellung.Loeschen.OK.0219',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        PlanfeststellungInitiierenNOK0221::class =>
+            [
+                'code' => '0221',
+                'name' => 'planfeststellung.Initiieren.NOK.0221',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        PlanfeststellungAktualisierenNOK0222::class =>
+            [
+                'code' => '0222',
+                'name' => 'planfeststellung.Aktualisieren.NOK.0222',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        PlanfeststellungLoeschenNOK0229::class =>
+            [
+                'code' => '0229',
+                'name' => 'planfeststellung.Loeschen.NOK.0229',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        RaumordnungInitiierenOK0311::class =>
+            [
+                'code' => '0311',
+                'name' => 'raumordnung.Initiieren.OK.0311',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        RaumordnungAktualisierenOK0312::class =>
+            [
+                'code' => '0312',
+                'name' => 'raumordnung.Aktualisieren.OK.0312',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        RaumordnungLoeschenOK0319::class =>
+            [
+                'code' => '0319',
+                'name' => 'raumordnung.Loeschen.OK.0319',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        RaumordnungInitiierenNOK0321::class =>
+            [
+                'code' => '0321',
+                'name' => 'raumordnung.Initiieren.NOK.0321',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        RaumordnungAktualisierenNOK0322::class =>
+            [
+                'code' => '0322',
+                'name' => 'raumordnung.Aktualisieren.NOK.0322',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
+            ],
+        RaumordnungLoeschenNOK0329::class =>
+            [
+                'code' => '0329',
+                'name' => 'raumordnung.Loeschen.NOK.0329',
+                'author' => 'Demosplan',
+                'recipient' => 'DiPlanCockpit',
             ],
     ];
 
