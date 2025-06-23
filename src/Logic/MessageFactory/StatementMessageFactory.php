be93b46a635a75c225269321dbbe396349f694e5
breierDemos <breier@demos-deutschland.de>
2025-06-23 13:05:00 +0200

    feat (refs DPLAN-15870): remove double xmlns

--- src/Logic/MessageFactory/StatementMessageFactory.php
+++ src/Logic/MessageFactory/StatementMessageFactory.php
@@ -280,8 +280,8 @@ class StatementMessageFactory extends XBeteiligungResponseMessageFactory
     {
         $simpleXML = simplexml_load_string($xml);
 
-        $simpleXML->addAttribute('xmlns:xmlns:xsi', 'https://www.w3.org/2001/XMLSchema-instance');
-        $simpleXML->addAttribute('xmlns:xmlns:gml', 'https://www.opengis.net/gml/3.2');
+        $simpleXML->addAttribute('xmlns:xsi', 'https://www.w3.org/2001/XMLSchema-instance');
+        $simpleXML->addAttribute('xmlns:gml', 'https://www.opengis.net/gml/3.2');
 
         $result = $simpleXML->asXML();
         if (!is_string($result)) {
