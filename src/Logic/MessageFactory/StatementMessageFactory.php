30e657e120465fec6cac74223d8805924b3ccdaf
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2025-09-30 11:25:55 +0200

    feat(refs DPLAN-15365): Move logic of creating Verfasser to another file

--- src/Logic/MessageFactory/StatementMessageFactory.php
+++ src/Logic/MessageFactory/StatementMessageFactory.php
@@ -7,10 +7,6 @@ use DemosEurope\DemosplanAddon\XBeteiligung\Configuration\Permissions\Features;
 use DemosEurope\DemosplanAddon\XBeteiligung\Exeption\NamespaceAdditionException;
 use DemosEurope\DemosplanAddon\XBeteiligung\Exeption\ProjectPrefixNotFoundException;
 use DemosEurope\DemosplanAddon\XBeteiligung\Logic\SerializerFactory;
-use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Kernmodul\AllgemeinerNameType;
-use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Kernmodul\AnschriftType;
-use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Kernmodul\KommunikationType;
-use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\Kernmodul\NameNatuerlichePersonType;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\AllgemeinStellungnahmeNeuabgegeben0701\AllgemeinStellungnahmeNeuabgegeben0701AnonymousPHPType\NachrichteninhaltAnonymousPHPType;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\AllgemeinStellungnahmeNeuabgegeben0701;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\BeteiligungKommunalTOEBType;
@@ -25,10 +21,9 @@ use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\CodeVerfahr
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\CodeVerfahrensschrittPlanfeststellungType;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\CodeVerfahrensschrittRaumordnungType;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\CodeVerfahrensteilschrittType;
-use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\OrganisationType;
 use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\StellungnahmeType;
-use DemosEurope\DemosplanAddon\XBeteiligung\Soap\Schema\XBeteiligung\VerfasserType;
 use DemosEurope\DemosplanAddon\XBeteiligung\ValueObject\StatementCreated;
+use DemosEurope\DemosplanAddon\XBeteiligung\Logic\MessageFactory\VerfasserBuilder;
 use DemosEurope\DemosplanAddon\XBeteiligung\XBeteiligungAsyncAddon;
 use Exception;
 use JsonException;
@@ -44,7 +39,6 @@ class StatementMessageFactory extends XBeteiligungResponseMessageFactory
     private const DEFAULT_PRIORITY_CODE = '3'; // "nicht vergeben" - priority not assigned
     private const DEFAULT_CONSIDERATION_CODE = '5000'; // "Die Stellungnahme wird zur Kenntnis genommen."
 
-   private const PRIVATE_PERSON  = 'Privatperson';
     /**
      * Builds a valid XBeteiligungsmessage as a response of creating a statement.
      *
@@ -88,7 +82,8 @@ class StatementMessageFactory extends XBeteiligungResponseMessageFactory
         $status->setCode($this->statusDerStellungnahme($statementCreated->getStatus()));
         $statement->setStatus($status);
         // set Verfasser --> user data
-        $verfasser = $this->buildVerfasser($statementCreated);
+        $verfasserBuilder = new VerfasserBuilder();
+        $verfasser = $verfasserBuilder->buildVerfasser($statementCreated);
         $statement->setVerfasser($verfasser);
 
         // set title
@@ -138,11 +133,6 @@ class StatementMessageFactory extends XBeteiligungResponseMessageFactory
         $nachricht->setVorgangsID($this->commonHelpers->uuid());
 
         return $nachricht;
-    }
-
-    private function getTypeOfPerson(StatementCreated $statementCreated): bool
-    {
-        return self::PRIVATE_PERSON === $statementCreated->getMeta()->getOrgaName();
     }
 
     private function statusDerStellungnahme($statusDerStellungnahme): string
@@ -311,147 +301,5 @@ class StatementMessageFactory extends XBeteiligungResponseMessageFactory
         return $result;
     }
 
-    private function buildVerfasser(StatementCreated $statementCreated): VerfasserType
-    {
-        $verfasser = new VerfasserType();
-
-        $this->setVerfasserType($verfasser, $statementCreated);
-        $this->setPersonalDetails($verfasser, $statementCreated);
-        $this->setAddress($verfasser, $statementCreated);
-
-        return $verfasser;
-    }
-
-    private function setVerfasserType(VerfasserType $verfasser, StatementCreated $statementCreated): void
-    {
-        if ($this->getTypeOfPerson($statementCreated)) {
-            $verfasser->setPrivatperson(true);
-        } else {
-            $organisation = new OrganisationType();
-            $organisation->setName($statementCreated->getMeta()->getOrgaName());
-            $verfasser->setOrganisation($organisation);
-        }
-    }
-
-    private function setPersonalDetails(VerfasserType $verfasser, StatementCreated $statementCreated): void
-    {
-        $user = $statementCreated->getUser();
-        $naturalPerson = new NameNatuerlichePersonType();
-
-        if (null !== $user && self::PRIVATE_PERSON !== $user->getFirstname()) {
-            $this->setPersonalDetailsFromUser($naturalPerson, $user);
-        } else {
-            $this->setPersonalDetailsFromMeta($naturalPerson, $statementCreated);
-        }
-
-        $verfasser->setName($naturalPerson);
-    }
-
-    private function setPersonalDetailsFromUser(NameNatuerlichePersonType $naturalPerson, $user): void
-    {
-        if (!empty($user->getTitle())) {
-            $naturalPerson->setTitel($user->getTitle());
-        }
-
-        if (!empty($user->getGender())) {
-            $naturalPerson->setAnrede($user->getGender());
-        }
-
-        $firstName = $this->createAllgemeinerName($user->getFirstname());
-        $lastName = $this->createAllgemeinerName($user->getLastname());
-
-        $naturalPerson->setVorname($firstName);
-        $naturalPerson->setFamilienname($lastName);
-    }
-
-    private function setPersonalDetailsFromMeta(NameNatuerlichePersonType $naturalPerson, StatementCreated $statementCreated): void
-    {
-        $authorName = $statementCreated->getMeta()->getAuthorName();
-        $firstName = $this->createAllgemeinerName($authorName);
-        $lastName = $this->createAllgemeinerName($authorName);
-
-        $naturalPerson->setVorname($firstName);
-        $naturalPerson->setFamilienname($lastName);
-    }
-
-    private function createAllgemeinerName(string $name): AllgemeinerNameType
-    {
-        $nameType = new AllgemeinerNameType();
-        $nameType->setName($name);
-
-        return $nameType;
-    }
-
-    private function setAddress(VerfasserType $verfasser, StatementCreated $statementCreated): void
-    {
-        $user = $statementCreated->getUser();
-
-        if (null !== $user && self::PRIVATE_PERSON !== $user->getFirstname()) {
-            $this->setAddressFromUser($verfasser, $user);
-        } else {
-            $this->setAddressFromMeta($verfasser, $statementCreated->getMeta());
-        }
-    }
-
-    private function setAddressFromUser(VerfasserType $verfasser, $user): void
-    {
-        $address = new AnschriftType();
-        $hasAddressData = false;
-        // Use user address data
-        $userAddress = $user->getAddress();
-
-        if (!empty($userAddress->getStreet())) {
-            $address->setStrasse($userAddress->getStreet());
-            $hasAddressData = true;
-        }
-
-        if (!empty($userAddress->getHouseNumber())) {
-            $address->setHausnummer($userAddress->getHouseNumber());
-            $hasAddressData = true;
-        }
-
-        if (!empty($userAddress->getPostalCode())) {
-            $address->setPostfach($userAddress->getPostalCode());
-            $hasAddressData = true;
-        }
-
-        if (!empty($userAddress->getCity())) {
-            $address->setOrt($userAddress->getCity());
-            $hasAddressData = true;
-        }
-
-        if ($hasAddressData) {
-            $verfasser->setAnschrift($address);
-        }
-    }
-
-    private function setAddressFromMeta(VerfasserType $verfasser, $meta): void
-    {
-        $address = new AnschriftType();
-        $hasAddressData = false;
-        if (!empty($meta->getOrgaStreet())) {
-            $address->setStrasse($meta->getOrgaStreet());
-            $hasAddressData = true;
-        }
-
-        if (!empty($meta->getHouseNumber())) {
-            $address->setHausnummer($meta->getHouseNumber());
-            $hasAddressData = true;
-        }
-
-        if (!empty($meta->getOrgaPostalCode())) {
-            $address->setPostfach($meta->getOrgaPostalCode());
-            $hasAddressData = true;
-        }
-
-        if (!empty($meta->getOrgaCity())) {
-            $address->setOrt($meta->getOrgaCity());
-            $hasAddressData = true;
-        }
-
-        if ($hasAddressData) {
-            $verfasser->setAnschrift($address);
-        }
-    }
 
 }
