79a950e369cb91893e478e967583aa5ed95b3ed3
breierDemos <breier@demos-deutschland.de>
2026-05-26 13:22:50 +0200

    chore DPLAN-16766: make Version20260424120741 idempotent

--- src/DoctrineMigrations/2026/04/Version20260424120741.php
+++ src/DoctrineMigrations/2026/04/Version20260424120741.php
@@ -16,13 +16,11 @@ final class Version20260424120741 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE xbeteiligung_phase_definition_code (id CHAR(36) NOT NULL, phase_definition_id CHAR(36) NOT NULL, code VARCHAR(100) NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, modified_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, UNIQUE INDEX UNIQ_20D12CF58EFDFE33 (phase_definition_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE xbeteiligung_phase_definition_code ADD CONSTRAINT FK_20D12CF58EFDFE33 FOREIGN KEY (phase_definition_id) REFERENCES procedure_phase_definition (id)');
+        $this->addSql('CREATE TABLE IF NOT EXISTS xbeteiligung_phase_definition_code (id CHAR(36) NOT NULL, phase_definition_id CHAR(36) NOT NULL, code VARCHAR(100) NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, modified_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, UNIQUE INDEX UNIQ_20D12CF58EFDFE33 (phase_definition_id), PRIMARY KEY(id), CONSTRAINT FK_20D12CF58EFDFE33 FOREIGN KEY (phase_definition_id) REFERENCES procedure_phase_definition (id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE xbeteiligung_phase_definition_code DROP FOREIGN KEY FK_20D12CF58EFDFE33');
-        $this->addSql('DROP TABLE xbeteiligung_phase_definition_code');
+        $this->addSql('DROP TABLE IF EXISTS xbeteiligung_phase_definition_code');
     }
 }
