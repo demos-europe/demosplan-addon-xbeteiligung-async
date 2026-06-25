b709670883dd1a0523e4b95281ec7a035822eb00
breierDemos <breier@demos-deutschland.de>
2026-06-25 12:47:40 +0200

    feat(refs DPLAN-18064): Delete XBeteiligungPhaseDefinitionCode when phase definition is soft-deleted
    Subscribe to ProcedurePhaseDefinitionMarkedAsDeletedEventInterface and remove
    the related XBeteiligungPhaseDefinitionCode mapping if one exists. The removal
    is scheduled within the same transaction as the soft-delete so both are atomic.

---
+++ src/EventSubscriber/XBeteiligungPhaseDefinitionCodeSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace DemosEurope\DemosplanAddon\XBeteiligung\EventSubscriber;
+
+use DemosEurope\DemosplanAddon\Contracts\Events\ProcedurePhaseDefinitionMarkedAsDeletedEventInterface;
+use DemosEurope\DemosplanAddon\XBeteiligung\Repository\XBeteiligungPhaseDefinitionCodeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class XBeteiligungPhaseDefinitionCodeSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly XBeteiligungPhaseDefinitionCodeRepository $repository,
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ProcedurePhaseDefinitionMarkedAsDeletedEventInterface::class => 'onPhaseDefinitionMarkedAsDeleted',
+        ];
+    }
+
+    public function onPhaseDefinitionMarkedAsDeleted(ProcedurePhaseDefinitionMarkedAsDeletedEventInterface $event): void
+    {
+        $code = $this->repository->findOneByPhaseDefinition($event->getPhaseDefinition());
+        if (null !== $code) {
+            $this->entityManager->remove($code);
+        }
+    }
+}
