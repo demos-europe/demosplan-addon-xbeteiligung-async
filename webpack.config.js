ef9b9e89a5dcba393c10f1f88e652b60a54bb0c9
Ania Rafeld <rafeld@demos-deutschland.de>
2026-07-13 19:36:23 +0200

    feat(refs DPLAN-18120): Add portal phase dropdown to create form and rename file
    - update addon yml and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const DemosPlanAddon = require('@demos-europe/demosplan-addon-client-builder')
 const config = DemosPlanAddon.build(
   'demosplan-addon-xbeteiligung-async',
   {
-    PhaseCodeFormField: DemosPlanAddon.resolve(
-      'client/hooks/PhaseCreateForm/PhaseCodeFormField.vue'
+    XBeteiligungCreateFormFields: DemosPlanAddon.resolve(
+      'client/hooks/PhaseCreateForm/XBeteiligungCreateFormFields.vue'
     ),
     PhaseCodeTableField: DemosPlanAddon.resolve(
       'client/hooks/PhaseListFields/PhaseCodeTableField.vue'
